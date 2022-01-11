07dc9dee86dd169bca7271686cc8c976e56b2705
Serra <pacoserranox@gmail.com>
2022-01-11 16:54:14 +0100

    Add project files.

---
+++ MvcCoreCrudDepartamentosEF2022/Controllers/DepartamentosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreCrudDepartamentosEF2022.Models;
+using MvcCoreCrudDepartamentosEF2022.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreCrudDepartamentosEF2022.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private RepositoryDepartamentos repo;
+
+        public DepartamentosController(RepositoryDepartamentos repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            List<Departamento> departamentos = this.repo.GetDepartamentos();
+            return View(departamentos);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Departamento departamento = this.repo.FindDepartamento(id);
+            return View(departamento);
+        }
+    }
+}
---
+++ MvcCoreCrudDepartamentosEF2022/Repositories/RepositoryDepartamentos.cs
@@ -0,0 +1,34 @@
+﻿using MvcCoreCrudDepartamentosEF2022.Data;
+using MvcCoreCrudDepartamentosEF2022.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreCrudDepartamentosEF2022.Repositories
+{
+    public class RepositoryDepartamentos
+    {
+        private DepartamentosContext context;
+
+        public RepositoryDepartamentos(DepartamentosContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Departamento> GetDepartamentos()
+        {
+            var consulta = from datos in this.context.Departamentos
+                           select datos;
+            return consulta.ToList();
+        }
+
+        public Departamento FindDepartamento(int id)
+        {
+            var consulta = from datos in this.context.Departamentos
+                           where datos.IdDepartamento == id
+                           select datos;
+            return consulta.FirstOrDefault();
+        }
+    }
+}
