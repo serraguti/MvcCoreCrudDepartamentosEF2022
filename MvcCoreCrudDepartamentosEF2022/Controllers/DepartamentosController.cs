2c531bc39c65edfe8b3192715ad65d767cef4bd5
Serra <pacoserranox@gmail.com>
2022-01-11 17:47:59 +0100

    commitCOMMIT

--- MvcCoreCrudDepartamentosEF2022/Controllers/DepartamentosController.cs
+++ MvcCoreCrudDepartamentosEF2022/Controllers/DepartamentosController.cs
@@ -28,5 +28,44 @@ namespace MvcCoreCrudDepartamentosEF2022.Controllers
             Departamento departamento = this.repo.FindDepartamento(id);
             return View(departamento);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult Create(Departamento departamento)
+        {
+            this.repo.InsertarDepartamento
+                (departamento.IdDepartamento, departamento.Nombre
+                , departamento.Localidad);
+            //DESPUES DE INSERTAR, LLEVAMOS A LA VISTA DE 
+            //MOSTRAR TODOS LOS DEPARTAMENTOS
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            this.repo.EliminarDepartamento(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Departamento departamento = this.repo.FindDepartamento(id);
+            return View(departamento);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Departamento departamento)
+        {
+            this.repo.ModificarDepartamento
+                (departamento.IdDepartamento, departamento.Nombre
+                , departamento.Localidad);
+            return RedirectToAction("Index");
+        }
     }
 }
--- MvcCoreCrudDepartamentosEF2022/Repositories/RepositoryDepartamentos.cs
+++ MvcCoreCrudDepartamentosEF2022/Repositories/RepositoryDepartamentos.cs
@@ -30,5 +30,44 @@ namespace MvcCoreCrudDepartamentosEF2022.Repositories
                            select datos;
             return consulta.FirstOrDefault();
         }
+
+        public void InsertarDepartamento
+            (int id, string nombre, string localidad)
+        {
+            //CREAMOS UNA NUEVA CLASE DEL MODELO
+            Departamento departamento = new Departamento();
+            //ASIGNAMOS SUS PROPIEDADES
+            departamento.IdDepartamento = id;
+            departamento.Nombre = nombre;
+            departamento.Localidad = localidad;
+            //AÑADIMOS A LA COLECCION DbSet LA 
+            //NUEVA CLASE CREADA
+            this.context.Departamentos.Add(departamento);
+            //GUARDAMOS LOS CAMBIOS EN LA BBDD
+            this.context.SaveChanges();
+        }
+
+        public void EliminarDepartamento(int id)
+        {
+            //BUSCAMOS EL DEPARTAMENTO/S A ELIMINAR
+            Departamento departamento = this.FindDepartamento(id);
+            //ELIMINAMOS EL DEPARTAMENTO DE LA COLECCION
+            //DbSet DEL CONTEXT
+            this.context.Departamentos.Remove(departamento);
+            //GUARDAMOS CAMBIOS
+            this.context.SaveChanges();
+        }
+
+        public void ModificarDepartamento(int id, string nombre
+            , string localidad)
+        {
+            //BUSCAMOS EL DEPARTAMENTO/S POR SU ID
+            Departamento departamento = this.FindDepartamento(id);
+            //CAMBIAMOS SUS PROPIEDADES (NO PK)
+            departamento.Nombre = nombre;
+            departamento.Localidad = localidad;
+            //GUARDAMOS CAMBIOS
+            this.context.SaveChanges();
+        }
     }
 }
